e44f29b37883bb14789fbc4d7d81cc57de63d8ff
brisa_alvarez <brisaorianaalvarez@outlook.com>
2022-10-14 17:02:34 -0300

    :sparkles: Feat: Crate a new repository with the first JS

---
+++ script.js
@@ -0,0 +1,55 @@
+//Type: String
+var name= "Brisa";
+console.log(name);
+
+//Type: Number
+var age= 18;
+console.log(age);
+
+//Type: Boolean
+var haveBoyfriend= true;
+console.log("Está en pareja: "+ haveBoyfriend)
+
+//Type: Boolean
+var haveSons= false;
+console.log("Tiene hijos:"+ haveSons);
+
+
+//Type: Object
+//Object with strings, numbers and booleans
+var user={
+   name: "Brisa",
+   age:18,
+   location: "Pilar, Buenos Aires",
+   haveBoyfriend:true,
+   haveSons:false,
+   havePets: true,
+   haveBrothers: true,
+   haveSisters:true,
+//Type: Array of strings and numbers
+   infoPets:[ petOne= {type: "dog",
+                       name: "Princesa",
+                       age: 1,
+                       race: "salchicha"},
+              petSecond= {type: "cat",
+                          name: "Nino",
+                          age: 3}],
+   infoBrothers:[ brotherOne={ name: "Brian",
+                               age: 23,
+                               favoriteColor: "Blue"},
+                  brotherSecond={name: "Francisco",
+                                 age: 27,
+                                 favoriteColor: "Red"}],
+   infoSisters:[ sisterOne= {name:"Paloma",
+                             age: 11,
+                             favoriteColor: "Pink"},
+                 sisterSecond= {name: "Loana",
+                                age: 25,
+                                favoriteColor: "Blue"}]
+}
+console.log(user)
+
+
+
+
+
